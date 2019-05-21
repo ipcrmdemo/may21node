b67786f1968e477b3d9033ca65518e50f1ba0706
Matt Cadorette <mcadorette@gmail.com>
2019-05-21 10:47:21 -0400

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/may21node/May21nodeApplication.java
@@ -0,0 +1,12 @@
+package com.may21node;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class May21nodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(May21nodeApplication.class, args);
+	}
+}
